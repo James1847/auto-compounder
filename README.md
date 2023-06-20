56c3708770b05c85c24b3a2b846dc04b4c1fa582
James <1536755789@qq.com>
2023-06-20 21:05:34 +0800

    complete readme.md and fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# auto-compounder
-the smart contract of auto-compounder for HailStone labs 
+# AutoCompounder
+
+`AutoCompounder`is a Solidity smart contract deployed on the Binance Smart Chain (BSC) testnet. This contract automates the process of compounding rewards for users by interacting with other contracts such as the Wombat MasterChefV2, PoolV2, and PancakeSwap Router V3.
+
+## Installation
+
+To install`AutoCompounder`,  follow these steps:
+- Clone this repository to your local machine
+- Install dependencies by running `npm install`
+- All the smart contracts in the `contracts` folder
+
+## Running Tests
+- Run hardhat test cases by running `npx hardhat test --network bscTestNet`
+
+## About the Contract
+The AutoCompounder contract has the following features:
+
+- Minting faucet USDC for testing purposes (only callable by the contract owner).
+- Depositing USDC to PoolV2 to receive USDC-LP tokens.
+- Withdrawing USDC from PoolV2 using USDC-LP tokens.
+- Staking LP tokens in MasterWombatV2 to earn rewards.
+- Depositing and staking USDC in one function call.
+- Checking pending WOM token rewards for a user.
+- Checking the staked USDC-LP token amount of a user.
+- Auto-compounding rewards by swapping WOM tokens to USDC, depositing USDC back to PoolV2 to get LP tokens, and staking those LP tokens in MasterWombatV2.
+
+## Time Spent
+- 5.19 10am to 6pm -> write smart contract and read the doc、analyze the verified smart contracts: 8 hours total
+- 5.20 0am to 3am -> almost complete all the logic of the smart contract and test it using remix: 3 hours total
+- 5.20 9am to 9pm -> write test cases to verify the whole process is correct or not, check contract security issues: 12 hours total
+- total: 8 + 3 + 12 = 23 hours
+
+## Improvements
+If I had more time, I would improve the following aspects of the `AutoCompounder` contract:
+- The deposit and withdraw function I will make these functions called by fallback function, because for some mev hackers, the will monitor 
+this contract's pending transactions all the time, then decode the function input if they have the contract abi. If they can know who is going
+to deposit or withdraw with a lot of money, they will insert their transactions before the user's transaction by using some mev tools such as 
+flashbots. So if we can hide the input data by calling the fallback function and the function selector is contained in the input data, the mev
+hackers are so hard to track who is going to deposit or withdraw.
+
+
+- Try to use more efficient ways to swap wom token to usdc, cause in the current contract, I just swap it from pancake v3 wom-usdc 0.1% pool directly.
+this will incur some serious problems such as Price Manipulation Attacks，So if we can use chainLink oracle price or some other tools to calculate the best
+route to swap the token, I can make this contract more safe.
+
+
+- Add more comprehensive tests to cover edge cases and potential vulnerabilities.
+
+
+- Implement more error handling to prevent unexpected behavior and improve user experience.
+
+
+- Add more documentation and comments to make the code more readable and understandable for other developers.
+
+
+- Optimize gas usage to reduce transaction costs for users.
+
--- contracts/AutoCompounder.sol
+++ contracts/AutoCompounder.sol
@@ -16,13 +16,12 @@ contract AutoCompounder is Ownable, ReentrancyGuard {
     address public constant lpTokenUSDCAddress = 0xA55741C7002FBdA046B0E8BC5e0D126f911Fa334; // Wombat Exchange Main pool’s LP-USDC on BSC testnet
     address public constant _usdc = 0x38dBDc4aa4D506CCDda88A08C3EdFf0a2636C2A5; // usdc token address on BSC testnet
     address public constant _wom = 0xeCa80fd2B6902C5447cC067f3d3Ce2670c50eB7E; // wom token address on BSC testnet
-    mapping(address => uint256) public depositedAmount;
-    IPoolV2 public poolV2 = IPoolV2(poolV2ProxyAddress);
-    IMasterWombatV2 public masterWombatV2 = IMasterWombatV2(womMasterChefV2Address);
-    TestERC20 public usdc = TestERC20(_usdc);
-    IERC20 public wom = IERC20(_wom);
-    IERC20 public usdcLP = IERC20(lpTokenUSDCAddress);
-    IV3SwapRouter public pancakeSwapRouter = IV3SwapRouter(pancakeSwapRouterAddress);
+    IPoolV2 public constant poolV2 = IPoolV2(poolV2ProxyAddress);
+    IMasterWombatV2 public constant masterWombatV2 = IMasterWombatV2(womMasterChefV2Address);
+    TestERC20 public constant usdc = TestERC20(_usdc);
+    IERC20 public constant wom = IERC20(_wom);
+    IERC20 public constant usdcLP = IERC20(lpTokenUSDCAddress);
+    IV3SwapRouter public constant pancakeSwapRouter = IV3SwapRouter(pancakeSwapRouterAddress);
 
     event Deposit(uint256 amount, uint256 lpReward);
     event Stake(uint256 amount, address receiver);
@@ -36,22 +35,25 @@ contract AutoCompounder is Ownable, ReentrancyGuard {
     }
 
     // @dev easy way to mint faucet usdc
-    function mintFaucetUSDC(uint256 _amount) external onlyOwner {
-        usdc.faucet(_amount);
-        usdc.transfer(msg.sender, _amount);
-        emit MintedFaucetUSDC(_amount);
+    function mintFaucetUSDC(uint256 amount) external nonReentrant onlyOwner {
+        usdc.faucet(amount);
+        bool success = usdc.transfer(msg.sender, amount);
+        require(success, "transfer failed");
+        emit MintedFaucetUSDC(amount);
     }
 
     // @dev deposit the usdc to pool to get lp token back
-    function depositToPool(uint256 _amount, uint256 _deadline) internal returns (uint256) {
-        require(_amount > 0, "Amount must be greater than 0");
+    function depositToPool(uint256 amount, uint256 deadline) internal returns (uint256) {
+        require(amount > 0, "Amount must be greater than 0");
         // transfer and approve usdc
-        usdc.transferFrom(msg.sender, address(this), _amount);
-        usdc.approve(poolV2ProxyAddress, _amount);
+        bool success = usdc.transferFrom(msg.sender, address(this), amount);
+        require(success, "usdc transfer failed");
+        bool approved = usdc.approve(poolV2ProxyAddress, amount);
+        require(approved, "usdc approve failed");
         // deposit to poolV2
-        uint256 lpReward = poolV2.deposit(_usdc, _amount, 0, address(this), _deadline, false);
+        uint256 lpReward = poolV2.deposit(_usdc, amount, 0, address(this), deadline, false);
         require(lpReward > 0, "lp token has been exploited");
-        emit Deposit(_amount, lpReward);
+        emit Deposit(amount, lpReward);
         return lpReward;
     }
 
@@ -59,47 +61,50 @@ contract AutoCompounder is Ownable, ReentrancyGuard {
     // @notice if we try to withdraw lp tokens from smart contract the msg.sender will be address(this), the amount to be withdraw will always be zero
     // @notice so user have to withdraw lp tokens by directly calling the MasterWombatV2 contract's withdraw function
     // @notice then user will call withdrawFromPool of this contract to withdraw his USDC by sending his lp tokens to this contract
-    function withdrawFromPool(uint256 _amount, uint256 _minimumAmountOut, uint256 _deadline) external returns (uint256) {
+    function withdrawFromPool(uint256 amount, uint256 minimumAmountOut, uint256 deadline) external nonReentrant returns (uint256) {
         // transfer usdc lp token from user to this contract
-        usdcLP.transferFrom(msg.sender, address(this), _amount);
+        bool transSuccess = usdcLP.transferFrom(msg.sender, address(this), amount);
+        require(transSuccess, "transfer failed");
 
         //approve usdcLP can be consumed by poolV2 contract
-        usdcLP.approve(address(poolV2), _amount);
+        bool approveSuccess = usdcLP.approve(address(poolV2), amount);
+        require(approveSuccess, "usdc approve failed");
         // try to withdraw USDC using lp token then send to msg.sender directly
-        uint256 withdrawAmount = poolV2.withdraw(address(usdc), _amount, _minimumAmountOut, msg.sender, _deadline);
+        uint256 withdrawAmount = poolV2.withdraw(address(usdc), amount, minimumAmountOut, msg.sender, deadline);
         emit Withdraw(withdrawAmount, msg.sender);
         return withdrawAmount;
 
     }
 
     // @dev stake usdc lp token to MasterWombatV2
-    function stakeToMasterWombat(uint256 _lpReward) internal {
+    function stakeToMasterWombat(uint256 lpReward) internal {
         uint256 _pid = masterWombatV2.getAssetPid(lpTokenUSDCAddress);
-        usdcLP.approve(address(masterWombatV2), _lpReward);
-        masterWombatV2.depositFor(_pid, _lpReward, msg.sender);
-        emit Stake(_lpReward, msg.sender);
+        bool approved = usdcLP.approve(address(masterWombatV2), lpReward);
+        require(approved, "lp approve failed");
+        masterWombatV2.depositFor(_pid, lpReward, msg.sender);
+        emit Stake(lpReward, msg.sender);
     }
 
     // @dev 1\ deposit usdc to poolV2 to get lpToken
     // @dev 2\ stake lpToken to MasterWombatV2
-    function depositAndStake(uint256 _amount, uint256 _deadline) public returns (uint256) {
-        uint256 lpReward = depositToPool(_amount, _deadline);
+    function depositAndStake(uint256 amount, uint256 deadline) public nonReentrant returns (uint256) {
+        uint256 lpReward = depositToPool(amount, deadline);
         require(lpReward > 0, "deposit failed");
         stakeToMasterWombat(lpReward);
         return lpReward;
     }
 
     // @dev get the amount of pending wom rewards from masterWombatV2
-    function getPendingWom(address _user) public view returns (uint256) {
+    function getPendingWom(address user) public view returns (uint256) {
         uint256 _pid = masterWombatV2.getAssetPid(lpTokenUSDCAddress);
-        (uint256 pendingWomAmount, , ,) = masterWombatV2.pendingTokens(_pid, _user);
+        (uint256 pendingWomAmount, , ,) = masterWombatV2.pendingTokens(_pid, user);
         return pendingWomAmount;
     }
 
     // @dev get staked lp token amount of user
-    function getStakedLp(address _user) public view returns (uint128) {
+    function getStakedLp(address user) public view returns (uint128) {
         uint256 _pid = masterWombatV2.getAssetPid(lpTokenUSDCAddress);
-        (uint128 stakedLp,,,) = masterWombatV2.userInfo(_pid, _user);
+        (uint128 stakedLp,,,) = masterWombatV2.userInfo(_pid, user);
         return stakedLp;
     }
 
@@ -125,13 +130,15 @@ contract AutoCompounder is Ownable, ReentrancyGuard {
         // run depositFor with amount 0, just to harvest
         masterWombatV2.depositFor(_pid, 0, msg.sender);
         // transfer wom token from user to this contract, because the depositFor function will transfer wom token to msg.sender
-        wom.transferFrom(msg.sender, address(this), pendingWomAmount);
+        bool success = wom.transferFrom(msg.sender, address(this), pendingWomAmount);
+        require(success, "wom transfer failed");
         // get token amount out from swap
         uint256 amountOutUSDC = swapFromPancakeV3(pendingWomAmount);
         uint256 usdcAllowance = usdc.allowance(msg.sender, address(this));
         require(usdcAllowance >= amountOutUSDC, "Not enough allowance for usdc token");
         // deposit again
-        usdc.transferFrom(msg.sender, address(this), amountOutUSDC);
+        bool transferred = usdc.transferFrom(msg.sender, address(this), amountOutUSDC);
+        require(transferred, "usdc transfer failed");
         uint256 deposited = depositAndStake(amountOutUSDC, block.timestamp + 100);
         emit AutoCompound(msg.sender, pendingWomAmount, deposited);
         return deposited;
@@ -140,7 +147,8 @@ contract AutoCompounder is Ownable, ReentrancyGuard {
     // swap wom token to usdc, we will use pancakeswap v3 router contract directly
     // @notice actually pancakeswap v3 router contract is forked from uniswap v3 router2 contract
     function swapFromPancakeV3(uint256 amountIn) internal nonReentrant returns (uint256 amountOut) {
-        wom.approve(address(pancakeSwapRouter), amountIn);
+        bool approved = wom.approve(address(pancakeSwapRouter), amountIn);
+        require(approved, "wom approve failed");
         // based on the task we have to swap tokens on PancakeSwap V3 WOM-USDC pool, which pool fee is 100 (0.1%)
         uint24 poolFee = 100;
         // @todo if we want swap as much amountOut as possible, maybe we have to get the best route from pancakeswap alpha router
